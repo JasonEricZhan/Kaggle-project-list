de74a7e4bf67e366f522b0d5246701a5d824a822
Chan Yao Chun(Eric) <jan11781202@gmail.com>
2018-03-26 23:11:02 +0800

    Update out-of-fold-cv.py

--- toxic_comment/out-of-fold-cv.py
+++ toxic_comment/out-of-fold-cv.py
@@ -1,74 +1,52 @@
 from sklearn.metrics import roc_auc_score
-K_fold=split
-pred_val_accumulator=test_accumulator=None
-accumulator=[]
+import numpy as np
 
-#Better for validation
-from sklearn.model_selection import KFold
+#train = train.sample(frac=1)
 
-kf = KFold(n_splits=10,shuffle=True,random_state=42)
+class_names = list(train)[-6:]
+multarray = np.array([100000, 10000, 1000, 100, 10, 1])
+y_multi = np.sum(train[class_names].values * multarray, axis=1)
 
-for train_index, val_index in kf.split(np.zeros(train.shape[0])):
-    c_train_x=X_tr[train_index]
-    c_train_y=y_tr[train_index]
-    c_val_X = X_tr[val_index]
-    c_val_y = y_tr[val_index]
+print(class_names)
+
+print(y_multi)
 
 
+from sklearn.model_selection import StratifiedKFold
+splits = 10
+skf = StratifiedKFold(n_splits=splits, shuffle=True, random_state=42)
 
-#Navie K-fold
+# produce two lists of ids. each list has n items where n is the 
+#    number of folds and each item is a pandas series of indexed id numbers
+train_ids = [] 
+val_ids = []
+for i, (train_idx, val_idx) in enumerate(skf.split(np.zeros(train.shape[0]), y_multi)):
+    train_ids.append(train.loc[train_idx, 'id'])
+    val_ids.append(train.loc[val_idx, 'id'])
+
+
+
 from sklearn.metrics import roc_auc_score
-K_fold=10
-pred_val_accumulator=test_accumulator=None
-accumulator=[]
+splits
 
-for i in range(1,int(K_fold+1)):
-    print("=================================")
-    print("Start on: "+str(i)+" fold")
-    length=int(len(X_tr)/K_fold)
-    if((len(X_tr)/K_fold)==length):
-        c_val_X=X_tr[length*(i-1):length*i]
-        c_val_y=y_tr[length*(i-1):length*i]
-    else:
-        c_val_X=X_tr[length*(i-1):length*i]
-        c_val_y=y_tr[length*(i-1):length*i]
-        if(i==K_fold):
-            c_val_X=X_tr[length*(i-1):]
-            c_val_y=y_tr[length*(i-1):]
+accumulator=[]
+for i in range(splits):
+    print("======")
+    print(str(i)+"fold")
+    print("======")
+    c_train_X = X_tr[train.id.isin(train_ids[i])]
+    c_train_y = y_tr[train.id.isin(train_ids[i])]
+    c_val_X = X_tr[train.id.isin(val_ids[i])]
+    c_val_y = y_tr[train.id.isin(val_ids[i])]       
+        #c_train_X_twitter=np.vstack((c_train_XOne_twitter,c_train_XTwo_twitter))
+        
+    """"
     
-    indexOne=length*(i-1)
-    indexTwo=length*(i)
     
-    if(i==1):
-        c_train_X=X_tr[indexTwo:]
-        c_train_y=y_tr[indexTwo:]
-        
-    elif(i==K_fold):
-        c_train_X=X_tr[:indexOne]
-        c_train_y=y_tr[:indexOne]
-    else:
-        c_train_XOne=X_tr[:indexOne]
-        c_train_yOne=y_tr[:indexOne]
-        c_train_XTwo=X_tr[indexTwo:]
-        c_train_yTwo=y_tr[indexTwo:]
-        
-        c_train_X=np.vstack((c_train_XOne,c_train_XTwo))
-        c_train_y=np.vstack((c_train_yOne,c_train_yTwo))
-        
-        
-        
-    print("")
-    file_path="weights_base_5_fold_CNN.hdf5"
-    checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-    early = EarlyStopping(monitor="val_loss", mode="min",verbose=1, patience=5)
-    callbacks_list = [early,checkpoint]
-    model = get_model()
-    seed(42)   #original is 1, change to 42 
-    history=model.fit(c_train_X,c_train_y, batch_size=batch_size, epochs=epochs, 
-                      validation_data=(c_val_X, c_val_y), callbacks=callbacks_list) #[c_train_X, c_train_X_gram]
-    #history=model.fit({'main_input':c_train_X, 'aux_input': c_train_X_twitter},c_train_y, batch_size=batch_size, epochs=epochs, 
-                      #validation_data=({'main_input':c_val_X, 'aux_input': c_val_X_twitter}, c_val_y),callbacks=callbacks_list)
+    NN model training
     
+    """
+    #record the curve plot
     import matplotlib
     matplotlib.use('Agg')
     import matplotlib.pyplot as plt
@@ -106,11 +84,13 @@ for i in range(1,int(K_fold+1)):
     #pred = model.predict( {'main_input':c_val_X, 'aux_input': c_val_X_twitter},batch_size=batch_size, verbose=1)
     y_test = model.predict( X_te_1,batch_size=batch_size, verbose=1)
    
-    if(i==1):
+    if(i==0):
          pred_val_accumulator=pred_val
          test_accumulator=y_test
     else:
+         #pred_accumulator is not None:
          pred_val_accumulator=np.vstack((pred_val_accumulator,pred_val))
+         #test_accumulator is not None:
          test_accumulator=test_accumulator+y_test
     sub_accumulator=[]
     for j in range(0,len(list_classes)):
@@ -119,9 +99,8 @@ for i in range(1,int(K_fold+1)):
         print("#Column: "+str(j)+" Roc_auc_score: "+str(roc_score))
         sub_accumulator.append(roc_score)
     print("#Average Roc_auc_score is: {}\n".format( np.mean(sub_accumulator) ))
-    #prevent the program break down(core dump), save to the disk at each step
-    pickle.dump(pred_val_accumulator,open("prediction_RNN_1_"+str(i)+".pkl", "wb"))
-    pickle.dump(test_accumulator,open("test_average_1_"+str(i)+".pkl", "wb"))
+    pickle.dump(pred_val_accumulator,open("OOF_"+str(i)+".pkl", "wb"))
+    pickle.dump(test_accumulator,open("test_average_"+str(i)+".pkl", "wb"))
     accumulator.append(np.mean(sub_accumulator))
     del model
 test_average=test_accumulator/K_fold
@@ -129,6 +108,7 @@ test_average=test_accumulator/K_fold
 print("#Total average Roc_auc_score is: {}\n".format( np.mean(accumulator) ))    
 
 
-pickle.dump(pred_val_accumulator,open("prediction_RNN.pkl", "wb"))
+pickle.dump(pred_val_accumulator,open("OOF_.pkl", "wb"))
 if test_accumulator is not None:
-   pickle.dump(test_average,open("test_average.pkl", "wb"))
+   pickle.dump(test_average,open("test_average_.pkl", "wb"))
+
