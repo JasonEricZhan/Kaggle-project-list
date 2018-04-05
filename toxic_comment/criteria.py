38d86c36228353c77c9b6d0c902069c7874e110d
Chan Yao Chun(Eric) <jan11781202@gmail.com>
2018-04-05 12:15:59 +0800

    Create criteria.py

---
+++ toxic_comment/criteria.py
@@ -0,0 +1,59 @@
+tf.keras.backend.set_session(sess)
+
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
+
+
+from numpy.random import seed
+seed(1)
+
+
+import keras.backend as K
+
+def f1_score(y_true, y_pred):
+
+    # Count positive samples.
+    c1 = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    c2 = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    c3 = K.sum(K.round(K.clip(y_true, 0, 1)))
+
+    # If there are no true samples, fix the F1 score at 0.
+    if c3 == 0:
+        return 0
+
+    # How many selected items are relevant?
+    precision = c1 / c2
+
+    # How many relevant items are selected?
+    recall = c1 / c3
+
+    # Calculate f1_score
+    f1_score = 2 * (precision * recall) / (precision + recall)
+    return f1_score        
+    
+
+def binary_PFA(y_true, y_pred, threshold=K.variable(value=0.5)):
+    y_pred = K.cast(y_pred >= threshold, 'float32')
+    # N = total number of negative labels
+    N = K.sum(1 - y_true)
+    # FP = total number of false alerts, alerts from the negative class labels
+    FP = K.sum(y_pred - y_pred * y_true)    
+    return FP/N
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+# P_TA prob true alerts for binary classifier
+def binary_PTA(y_true, y_pred, threshold=K.variable(value=0.5)):
+    y_pred = K.cast(y_pred >= threshold, 'float32')
+    # P = total number of positive labels
+    P = K.sum(y_true)
+    # TP = total number of correct alerts, alerts from the positive class labels
+    TP = K.sum(y_pred * y_true)    
+    return TP/P
+
+
+def auc(y_true, y_pred):   
+    ptas = tf.stack([binary_PTA(y_true,y_pred,k) for k in np.linspace(0, 1, 1000)],axis=0)
+    pfas = tf.stack([binary_PFA(y_true,y_pred,k) for k in np.linspace(0, 1, 1000)],axis=0)
+    pfas = tf.concat([tf.ones((1,)) ,pfas],axis=0)
+    binSizes = -(pfas[1:]-pfas[:-1])
+    s = ptas*binSizes
+    return K.sum(s, axis=0)
