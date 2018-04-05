8926c6844886302e98c4f974c2b370d26d7e52eb
Chan Yao Chun(Eric) <jan11781202@gmail.com>
2018-04-05 13:21:24 +0800

    Update stacking.py

--- toxic_comment/model/stacking.py
+++ toxic_comment/model/stacking.py
@@ -25,26 +25,11 @@ import pickle
 
 
 import pickle
+=======================
 
-test_ave_CNN=pickle.load(open("stack_pack/test_average_CNN.pkl", "rb"))
-test_ave_Capsule=pickle.load(open("stack_pack/test_average_Capsule.pkl", "rb"))
-test_ave_GRU=pickle.load(open("stack_pack/test_average_0.9854.pkl", "rb"))
-test_ave_LSTM=pickle.load(open("stack_pack/test_average_LSTM_0.9852.pkl", "rb"))
-#test_ave_GRU_2=pickle.load(open("test_average.pkl", "rb"))
-test_ave_GRU_3=pickle.load(open("test_ave_wiki_char_gru.pkl", "rb"))
-test_ave_lgbm=pd.read_csv('lvl0_lgbm_clean_sub.csv').iloc[:,1:].values
-test_ave_no_char=pickle.load(open("test_average__GRU_no_char9.pkl", "rb"))/10
+#........load the out of fold.......
 
-
-oof_Capsule=pickle.load(open("stack_pack/prediction_RNN_Capsule.pkl", "rb"))
-oof_CNN=pickle.load(open("stack_pack/prediction_DPCNN.pkl", "rb"))
-oof_GRU=pickle.load(open("stack_pack/prediction_RNN _0.9854.pkl", "rb"))
-#oof_GRU_2=pickle.load(open("prediction_RNN.pkl", "rb"))
-oof_GRU_3=pickle.load(open("wiki_char_oof.pkl", "rb"))
-oof_LSTM=pickle.load(open("stack_pack/prediction_RNN_LSTM_0.9852.pkl", "rb"))
-oof_GRU_no_char=pickle.load(open("oof_GRU_no_char.pkl", "rb"))
-oof_lgbm=pd.read_csv('lvl0_lgbm_clean_oof.csv').iloc[:,7:].values
-
+=======================
 class_names = list(train)[-6:]
 multarray = np.array([100000, 10000, 1000, 100, 10, 1])
 y_multi = np.sum(train[class_names].values * multarray, axis=1)
@@ -64,136 +49,17 @@ for i, (train_idx, val_idx) in enumerate(skf.split(np.zeros(train.shape[0]), y_m
     val_ids.append(train.loc[val_idx, 'id'])
     
     
-
-container_before=oof_CNN
-length_before=0
-length_after=0
-new=np.zeros((container_before.shape[0],container_before.shape[1]))
-for i in range(0,10):
-    length_after+=len(np.where(train.id.isin(train_ids[i]).values==0)[0])
-    if i ==9:
-       input_segment=container_before[length_before:]
-    else:
-       input_segment=container_before[length_before:length_after]
-    length_before+=len(np.where(train.id.isin(train_ids[i]).values==0)[0])
-    counter=0
-    for j in  np.where(train.id.isin(train_ids[i]).values==0)[0]:
-        new[j,:]=input_segment[counter,:]
-        counter+=1
-    #print(counter)
-     
-oof_CNN=new
-
-
-
-
-
-container_before=oof_Capsule
-length_before=0
-length_after=0
-new=np.zeros((container_before.shape[0],container_before.shape[1]))
-for i in range(0,10):
-    length_after+=len(np.where(train.id.isin(train_ids[i]).values==0)[0])
-    if i ==9:
-       input_segment=container_before[length_before:]
-    else:
-       input_segment=container_before[length_before:length_after]
-    length_before+=len(np.where(train.id.isin(train_ids[i]).values==0)[0])
-    counter=0
-    for j in  np.where(train.id.isin(train_ids[i]).values==0)[0]:
-        new[j,:]=input_segment[counter,:]
-        counter+=1
-    #print(counter)
-     
-oof_Capsule=new
-
-
-
-
-
-
-
-
-"""
-container_before=oof_GRU_3
-length_before=0
-length_after=0
-new=np.zeros((container_before.shape[0],container_before.shape[1]))
-for i in range(0,10):
-    length_after+=len(np.where(train.id.isin(train_ids[i]).values==0)[0])
-    if i ==9:
-       input_segment=container_before[length_before:]
-    else:
-       input_segment=container_before[length_before:length_after]
-    length_before+=len(np.where(train.id.isin(train_ids[i]).values==0)[0])
-    counter=0
-    for j in  np.where(train.id.isin(train_ids[i]).values==0)[0]:
-        new[j,:]=input_segment[counter,:]
-        counter+=1
-    #print(counter)
-     
-oof_GRU_3=new
-"""    
-
-
-
-
-container_before=oof_LSTM
-length_before=0
-length_after=0
-new=np.zeros((container_before.shape[0],container_before.shape[1]))
-for i in range(0,10):
-    length_after+=len(np.where(train.id.isin(train_ids[i]).values==0)[0])
-    if i ==9:
-       input_segment=container_before[length_before:]
-    else:
-       input_segment=container_before[length_before:length_after]
-    length_before+=len(np.where(train.id.isin(train_ids[i]).values==0)[0])
-    counter=0
-    for j in  np.where(train.id.isin(train_ids[i]).values==0)[0]:
-        new[j,:]=input_segment[counter,:]
-        counter+=1
-    #print(counter)
-     
-oof_LSTM=new    
     
     
     
-    
-    
-    
-    
-    
-"""
-import lightgbm as lgb
-from sklearn.model_selection import cross_val_score
+=======================
 
-stacker = lgb.LGBMClassifier(max_depth=4, metric="auc", n_estimators=50, num_leaves=15, boosting_type="gbdt", learning_rate=0.01, feature_fraction=0.45, colsample_bytree=0.45, bagging_fraction=0.8, bagging_freq=5, reg_lambda=0.2
-                            )
+#........hash back.......
+
+=======================
 
 
-sub = pd.read_csv("sample_submission.csv")
-
-
-
-
-
-
-
-# Fit and submit
-scores = []
-for label in list_classes:
-    print(label)
-    score = cross_val_score(stacker, X_tr, train[label], cv=10, scoring='roc_auc')
-    print("AUC:", score)
-    scores.append(np.mean(score))
-    stacker.fit(X_tr, train[label])
-    sub[label] = stacker.predict_proba(X_te)[:,1]
-print("CV score:", np.mean(scores))
-
-sub.to_csv("ensemble.csv", index=False)
-
-"""
+#absord high correlated data
 
 
 absord=0.5*oof_CNN+oof_Capsule*0.5
@@ -204,9 +70,6 @@ absord_GRU=oof_GRU_no_char*0.5+oof_GRU_3*0.5
 absord_GRU_te=test_ave_GRU_3*0.5+test_ave_no_char*0.5
 
 
-
-#absord_GRU=0.5*oof_GRU_2+0.5*oof_GRU
-#absord_te_GRU=0.5*test_ave_GRU_2+0.5*test_ave_GRU
 
 print(absord.shape)
 print(oof_GRU.shape)
@@ -220,18 +83,6 @@ X_te=np.hstack((absord_te,test_ave_GRU,test_ave_LSTM,test_ave_lgbm,absord_GRU_te
 #test_ave_GRU
 
 
-gbm = xgb.XGBClassifier(
-n_estimators= 1000,
-max_depth= 3,
-min_child_weight= 2,
-gamma=0.9,                        
-   #subsample=0.8,
-   #colsample_bytree=0.8,
-objective= 'multi:softprob',
-   #nthread= -1,
-nthread=1,
-seed=10,
-scale_pos_weight=1)
 
 
 def runXGB(train_X, train_y, test_X, test_y=None, feature_names=None, seed_val=2017, num_rounds=500):
