7342b37577b0b156b9c644c43005baa729a6c23e
Chan Yao Chun(Eric) <jan11781202@gmail.com>
2018-04-10 00:02:56 +0800

    Update Single_GRU_glove_char.py

--- toxic_comment/model/Single_GRU_glove_char.py
+++ toxic_comment/model/Single_GRU_glove_char.py
@@ -1,6 +1,5 @@
 # This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
+
 
 
 
